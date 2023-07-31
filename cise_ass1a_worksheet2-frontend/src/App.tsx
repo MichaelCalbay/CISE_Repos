2820d9c9f08b9dee35c1ca257fbdbad96dad1a35
MichaelCalbay <vjc0781@autuni.ac.nz>
2023-07-31 21:43:30 +1200

    first commit

---
+++ cise_ass1a_worksheet2-frontend/src/App.tsx
@@ -0,0 +1,42 @@
+// import './App.css';
+
+
+// const App = () =>
+//     <div className="App">
+//     <h1>Welcome to CISE - the home of learning and fun</h1>
+//     </div>
+
+// export default App;
+import React, { Component } from "react";
+
+interface State {
+  count: number;
+}
+
+class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  makeIncrementer = (amount: number) => () =>
+    this.setState((prevState: State) => ({
+      count: prevState.count + amount,
+    }));
+
+  increment = this.makeIncrementer(1);
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>
+          Increment count
+        </button>
+      </div>
+    );
+  }
+}
+export default App;
